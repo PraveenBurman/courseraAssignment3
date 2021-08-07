13b30642c976c5f47a5df96200f482c04b943ac2
PraveenBurman <88330278+PraveenBurman@users.noreply.github.com>
2021-08-08 02:03:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ names[8]="laura";
 names[9]="jim";
 for(var i=0; i<names.length; i++){
     if(names[i].charAt()=='J' || names[i].charAt()=='j'){
-        console.log("Goodbye " + names[i]);
+        console.log("Good Bye " + names[i]);
     }
     else{
         console.log("Hello " + names[i]);
